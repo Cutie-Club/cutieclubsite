55dafc829ce14f01885419172350b0b1b35da9b6
Amber Holly <halcyonsilver@gmail.com>
2020-03-01 21:57:13 +0000

    Add argon2 password hashing
    - Add password hashing function
    - Hash a user's password on creation
    - Verify a password hash before logging in a user

--- backend/routes/root.js
+++ backend/routes/root.js
@@ -1,35 +1,58 @@
 const jwt = require("jsonwebtoken");
 const secretKey = "cutie club";
+const argon2 = require('argon2');
+
 const newToken = (id, username) =>
   jwt.sign({ id: id, user: username }, secretKey, {
     expiresIn: "1h"
   });
 
+const generateHash = (password) => {
+  return argon2.hash(password, {
+    type: argon2.argon2id,
+    // memoryCost: 2 ** 16,
+    // parallelism: 4,
+    // hashLength: 64,
+    // saltLength: 32,
+  });
+};
+
 module.exports = (app, upload, db) => {
   const users = db.get("users");
-
   app.post("/newuser", upload.single("image"), (req, res) => {
     const url = "http://" + req.get("host");
     // validate data
-    // add to db
+    // check it an email, maybe send an email to check it exists
+    // check password meets requirements
+    // check email unique
+    // check username unique
+    // run through file processer
     let newBody = { ...req.body };
     if (req.file) {
       newBody["image"] = `${url}/${req.file.filename}`;
     }
-    newBody["pw_hash"] = newBody.password;
-    delete newBody.password;
 
-    users.get("email", newBody.email).then(user => {
-      if (user[0]) {
-        console.log("tthois users aalreddi exists :/");
-        return res.send("EXISTS");
-      }
-      users.create(newBody).then(result => {
-        console.log(result);
-        return res.send(newToken(result.insertId, newBody.username));
+    generateHash(newBody.password).then(hash => {
+      newBody["pw_hash"] = hash;
+      delete newBody.password;
+
+      users.get("email", newBody.email).then(user => {
+        if (user[0]) {
+          console.log("tthois users aalreddi exists :/");
+          return res.send("EXISTS");
+        }
+        users.create(newBody).then(result => {
+          console.log(result);
+          return res.send(newToken(result.insertId, newBody.username));
+        });
       });
     });
   });
+
+  // TODO:
+  // output from create/login
+  // {status: "success", data: "token"}
+  // {status: "error", data: "error message"}
 
   app.post("/login", upload.none(), (req, res) => {
     const username = req.body.username;
@@ -38,12 +61,16 @@ module.exports = (app, upload, db) => {
     users.get("username", username).then(([user]) => {
       if (!user) {
         return console.warn(`no user found matching ${username}`);
+        // inform user that username wasn't found
       }
-      if (password !== user.pw_hash) {
-        return console.warn(`login failure for ${username}!`);
-      }
-      console.log(`successful login for ${username}`);
-      return res.send(newToken(user.id, username));
+      argon2.verify(user.pw_hash, password).then((result) => {
+        if (!result) {
+          return console.warn(`login failure for ${username}!`);
+          // to frontend: inform user of a layer 8 error
+        }
+        console.log(`successful login for ${username}`);
+        return res.send(newToken(user.id, username));
+      })
     });
   });
 
--- frontend/src/pages/Admin.js
+++ frontend/src/pages/Admin.js
@@ -12,3 +12,10 @@ function Admin() {
 }
 
 export default Admin;
+
+// check sessionStorage for a token
+// if token, user is logged in
+// if not, redirect to /login
+// is logged in user admin?
+// if yes, render admin page
+// if no, return "you are not authorised to view this content!"
